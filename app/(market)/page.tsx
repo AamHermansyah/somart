682a082ceca03874cf9d02d741190be4cf95d51a
Aam Hermansyah <amzhermanzyah@gmail.com>
2024-05-28 13:47:39 +0700

    [FIX] Add suspend in '/' and '/dashboard/products'

--- app/(market)/page.tsx
+++ app/(market)/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import { Carousel } from "./_components/carousel"
 import Hero from "./_components/hero"
 import Products from "./_components/products"
@@ -6,7 +7,9 @@ export default function Home() {
   return (
     <div className="space-y-10 overflow-x-hidden">
       <Carousel />
-      <Products />
+      <Suspense>
+        <Products />
+      </Suspense>
       <Hero />
     </div>
   );
--- app/dashboard/products/page.tsx
+++ app/dashboard/products/page.tsx
@@ -1,10 +1,12 @@
 import Products from '@/app/(market)/_components/products'
-import React from 'react'
+import React, { Suspense } from 'react'
 
 function ProductsPage() {
   return (
     <div className="p-6 rounded-xl bg-background shadow-sm space-y-4">
-      <Products hiddenAddToCart={true} />
+      <Suspense>
+        <Products hiddenAddToCart={true} />
+      </Suspense>
     </div>
   )
 }
